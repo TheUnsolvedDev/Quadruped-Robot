8966f8eca484fb5eef769f81918b5a2de8d5b919
Shuvrajeet Das <59395895+TheUnsolvedDev@users.noreply.github.com>
2021-06-26 09:38:29 +0530

    v2

--- arduino/body.ino
+++ arduino/body.ino
@@ -6,115 +6,105 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // you can also call it with a different address you want
 //Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
 
-// Depending on your servo make, the pulse width min and max may vary, you 
+// Depending on your servo make, the pulse width min and max may vary, you
 // want these to be as small/large as possible without hitting the hard stop
 // for max range. You'll have to tweak them as necessary to match the servos you
 // have!
-#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN 125 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 575 // this is the 'maximum' pulse length count (out of 4096)
 
 // our servo # counter
 uint8_t servonum = 0;
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("16 channel Servo test!");
+void setup()
+{
+	Serial.begin(9600);
+	Serial.println("16 channel Servo test!");
 
-  pwm.begin();
-  
-  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+	pwm.begin();
 
-  }
-
-void loop() {
-//  pwm.setPWM(0, 0, angleToPulse(120) );
-//  pwm.setPWM(1, 0, angleToPulse(110) );
-//  pwm.setPWM(2, 0, angleToPulse(110) );
-//  pwm.setPWM(3, 0, angleToPulse(100) );
-//  
-//  pwm.setPWM(4, 0, angleToPulse(10) );
-//  pwm.setPWM(6, 0, angleToPulse(10) );
-//  pwm.setPWM(5, 0, angleToPulse(10) );
-//  pwm.setPWM(7, 0, angleToPulse(180) );
-//
-
-
-  
-  pwm.setPWM(0, 0, angleToPulse(160));
-  pwm.setPWM(1, 0, angleToPulse(145));
-  pwm.setPWM(2, 0, angleToPulse(155));
-  pwm.setPWM(3, 0, angleToPulse(60) );
-  
-  pwm.setPWM(4, 0, angleToPulse(85) );
-  pwm.setPWM(5, 0, angleToPulse(55) );
-  pwm.setPWM(6, 0, angleToPulse(60) );
-  pwm.setPWM(7, 0, angleToPulse(150));
-
-  pwm.setPWM(8, 0, angleToPulse(90)  );
-  pwm.setPWM(9, 0, angleToPulse(120) );
-  pwm.setPWM(10, 0, angleToPulse(180));
-  pwm.setPWM(11, 0, angleToPulse(60) );
-  
-  delay(2000);
-  
-  rotation(30);
-          
+	pwm.setPWMFreq(60); // Analog servos run at ~60 Hz updates
 }
 
+void loop()
+{
+	//  pwm.setPWM(0, 0, angleToPulse(120) );
+	//  pwm.setPWM(1, 0, angleToPulse(110) );
+	//  pwm.setPWM(2, 0, angleToPulse(110) );
+	//  pwm.setPWM(3, 0, angleToPulse(100) );
+	//
+	//  pwm.setPWM(4, 0, angleToPulse(10) );
+	//  pwm.setPWM(6, 0, angleToPulse(10) );
+	//  pwm.setPWM(5, 0, angleToPulse(10) );
+	//  pwm.setPWM(7, 0, angleToPulse(180) );
+	//
+
+	pwm.setPWM(0, 0, angleToPulse(160));
+	pwm.setPWM(1, 0, angleToPulse(145));
+	pwm.setPWM(2, 0, angleToPulse(155));
+	pwm.setPWM(3, 0, angleToPulse(60));
+
+	pwm.setPWM(4, 0, angleToPulse(85));
+	pwm.setPWM(5, 0, angleToPulse(55));
+	pwm.setPWM(6, 0, angleToPulse(60));
+	pwm.setPWM(7, 0, angleToPulse(150));
+
+	pwm.setPWM(8, 0, angleToPulse(90));
+	pwm.setPWM(9, 0, angleToPulse(120));
+	pwm.setPWM(10, 0, angleToPulse(180));
+	pwm.setPWM(11, 0, angleToPulse(60));
+
+	delay(2000);
+
+	rotation(30);
+}
 
 void rotation(int rotate)
 {
-  //rotate leg 0 by angle (int)rotate
-  pwm.setPWM(4, 0, angleToPulse(115));
-  for( int angle = 90; angle>=90-rotate; angle -=10)
-   {  
-    pwm.setPWM(8, 0, angleToPulse(angle));  
-    delay(100);
-  }
-  pwm.setPWM(4, 0, angleToPulse(85));
-  delay(1000);
+	//rotate leg 0 by angle (int)rotate
+	pwm.setPWM(4, 0, angleToPulse(115));
+	for (int angle = 90; angle >= 90 - rotate; angle -= 10)
+	{
+		pwm.setPWM(8, 0, angleToPulse(angle));
+		delay(100);
+	}
+	pwm.setPWM(4, 0, angleToPulse(85));
+	delay(1000);
 
-  
-  
-  pwm.setPWM(5, 0, angleToPulse(95));
-  for( int angle = 120; angle>=120-rotate; angle -=10)
-   {  
-    pwm.setPWM(9, 0, angleToPulse(angle));
-    delay(100);
-  }
-  pwm.setPWM(5, 0, angleToPulse(60));
-  delay(1000);
+	pwm.setPWM(5, 0, angleToPulse(95));
+	for (int angle = 120; angle >= 120 - rotate; angle -= 10)
+	{
+		pwm.setPWM(9, 0, angleToPulse(angle));
+		delay(100);
+	}
+	pwm.setPWM(5, 0, angleToPulse(60));
+	delay(1000);
 
-  
-  
-  
-  pwm.setPWM(6, 0, angleToPulse(95));
-  for( int angle = 180; angle>=180-rotate; angle -=10)
-  {  
-    pwm.setPWM(10, 0, angleToPulse(angle));
-    delay(100);
-  }
-  pwm.setPWM(6, 0, angleToPulse(60));
-  delay(1000);
+	pwm.setPWM(6, 0, angleToPulse(95));
+	for (int angle = 180; angle >= 180 - rotate; angle -= 10)
+	{
+		pwm.setPWM(10, 0, angleToPulse(angle));
+		delay(100);
+	}
+	pwm.setPWM(6, 0, angleToPulse(60));
+	delay(1000);
 
-  
-  
-  
-  pwm.setPWM(7, 0, angleToPulse(110));
-  for( int angle = 60; angle>=60-rotate; angle -=10)
-  {  
-    pwm.setPWM(11, 0, angleToPulse(angle));
-    delay(100);
-  }
-  pwm.setPWM(7, 0, angleToPulse(150));
-  delay(1000);
- 
+	pwm.setPWM(7, 0, angleToPulse(110));
+	for (int angle = 60; angle >= 60 - rotate; angle -= 10)
+	{
+		pwm.setPWM(11, 0, angleToPulse(angle));
+		delay(100);
+	}
+	pwm.setPWM(7, 0, angleToPulse(150));
+	delay(1000);
 }
 
-
-int angleToPulse(int ang){
-   int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
-   Serial.print("Angle: ");Serial.print(ang);
-   Serial.print(" pulse: ");Serial.println(pulse);
-   return pulse;
+int angleToPulse(int ang)
+{
+	int pulse = map(ang, 0, 180, SERVOMIN, SERVOMAX); // map angle of 0 to 180 to Servo min and Servo max
+	Serial.print("Angle: ");
+	Serial.print(ang);
+	Serial.print(" pulse: ");
+	Serial.println(pulse);
+	return pulse;
 }
--- arduino/face.ino
+++ arduino/face.ino
@@ -6,85 +6,95 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define OLED_RESET     4 
-#define SCREEN_ADDRESS 0x3C 
+#define OLED_RESET 4
+#define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-void normal() {
-  display.clearDisplay();
-  display.fillCircle (50, 15, 12, WHITE); 
-  display.fillCircle (82, 20, 7, WHITE); 
-  display.display();
+void normal()
+{
+	display.clearDisplay();
+	display.fillCircle(50, 15, 12, WHITE);
+	display.fillCircle(82, 20, 7, WHITE);
+	display.display();
 }
 
-void annoy() {
-  display.clearDisplay();
-  display.drawFastHLine(40, 15, 20, WHITE);
-  display.drawFastHLine(72, 20, 15, WHITE);
-  display.display();
+void annoy()
+{
+	display.clearDisplay();
+	display.drawFastHLine(40, 15, 20, WHITE);
+	display.drawFastHLine(72, 20, 15, WHITE);
+	display.display();
 }
 
-
-void angry_annoy() {
-  display.clearDisplay();
-  display.fillCircle (42, 10, 20, WHITE); 
-  display.fillCircle (82, 10, 15, WHITE); 
-  display.fillRect (0, 0, 128, 15, BLACK); 
-  display.fillRect (0, 40, 128, 15, BLACK); 
-  display.display();
+void angry_annoy()
+{
+	display.clearDisplay();
+	display.fillCircle(42, 10, 20, WHITE);
+	display.fillCircle(82, 10, 15, WHITE);
+	display.fillRect(0, 0, 128, 15, BLACK);
+	display.fillRect(0, 40, 128, 15, BLACK);
+	display.display();
 }
 
-void sad() {
-  display.clearDisplay();
-  display.fillCircle (42, 10, 17, WHITE); 
-  display.fillCircle (82, 10, 17, WHITE); 
-  display.fillTriangle (0, 0, 0, 35, 78, 0, BLACK); 
-  display.fillTriangle (50, 0, 128, 35, 128, 0, BLACK); 
-  display.display();
+void sad()
+{
+	display.clearDisplay();
+	display.fillCircle(42, 10, 17, WHITE);
+	display.fillCircle(82, 10, 17, WHITE);
+	display.fillTriangle(0, 0, 0, 35, 78, 0, BLACK);
+	display.fillTriangle(50, 0, 128, 35, 128, 0, BLACK);
+	display.display();
 }
 
-void happy() {
-  display.clearDisplay();
-  display.fillCircle (42, 25, 15, WHITE); 
-  display.fillCircle (82, 25, 15, WHITE); 
-  display.fillCircle (42, 33, 20, BLACK); 
-  display.fillCircle (82, 33, 20, BLACK); 
-  display.display();
+void happy()
+{
+	display.clearDisplay();
+	display.fillCircle(42, 25, 15, WHITE);
+	display.fillCircle(82, 25, 15, WHITE);
+	display.fillCircle(42, 33, 20, BLACK);
+	display.fillCircle(82, 33, 20, BLACK);
+	display.display();
 }
 
-void angry() {
-  display.clearDisplay();
-  display.fillCircle (42, 10, 18, WHITE); 
-  display.fillCircle (82, 10, 12, WHITE); 
-  display.fillTriangle (0, 0, 54, 26, 118, 0, BLACK); 
-  display.display();
+void angry()
+{
+	display.clearDisplay();
+	display.fillCircle(42, 10, 18, WHITE);
+	display.fillCircle(82, 10, 12, WHITE);
+	display.fillTriangle(0, 0, 54, 26, 118, 0, BLACK);
+	display.display();
 }
-void (*func_ptr[6])(void) = {normal,annoy,angry_annoy,sad,happy,angry};
+void (*func_ptr[6])(void) = {normal, annoy, angry_annoy, sad, happy, angry};
 
-void setup() {
-  Serial.begin(9600);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-}
-
-
-void faces(){
-  while(true){
-    int num = rand()%1000;
-    display.display();
-    delay(200);
-    display.clearDisplay();
-    for(int i=0;i<7;i++)
-    {
-      (*func_ptr[rand()%6])();
-      delay(num);
-    }
-    display.clearDisplay();
-  }
+void setup()
+{
+	Serial.begin(9600);
+	if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+	{
+		Serial.println(F("SSD1306 allocation failed"));
+		for (;;)
+			; // Don't proceed, loop forever
+	}
 }
 
-void loop() {
-  faces();
+void faces()
+{
+	while (true)
+	{
+		int num = rand() % 1000;
+		display.display();
+		delay(200);
+		display.clearDisplay();
+		for (int i = 0; i < 7; i++)
+		{
+			(*func_ptr[rand() % 6])();
+			delay(num);
+		}
+		display.clearDisplay();
+	}
+}
+
+void loop()
+{
+	faces();
 }
