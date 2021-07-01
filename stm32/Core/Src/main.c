65432f76e972d83394c106400485bd158a13b012
Shuvrajeet Das <59395895+TheUnsolvedDev@users.noreply.github.com>
2021-07-01 06:53:43 +0530

    v2

--- stm32/Core/Src/main.c
+++ stm32/Core/Src/main.c
@@ -38,7 +38,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define SERVO_COUNT 4
+#define SERVO_COUNT 12
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -84,25 +84,23 @@ void StartDefaultTask(void *argument);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void start_normal(void *parameters)
+void start_normal()
 {
-	while(1)
-	{
-		  PCA9685_SetServoAngle(0, 110);
-		  PCA9685_SetServoAngle(1, 100);
-		  PCA9685_SetServoAngle(2, 110);
-		  PCA9685_SetServoAngle(3, 45);
-		  HAL_Delay(500);
-		  PCA9685_SetServoAngle(4, 60);
-		  PCA9685_SetServoAngle(5, 40);
-		  PCA9685_SetServoAngle(6, 95);
-		  PCA9685_SetServoAngle(7, 110);
-		  PCA9685_SetServoAngle(8, 75);
-		  PCA9685_SetServoAngle(9, 100);
-		  PCA9685_SetServoAngle(10, 125);
-		  PCA9685_SetServoAngle(11, 35);
-	}
-	vTaskDelete(NULL);
+
+	  PCA9685_SetServoAngle(0, 110);
+	  PCA9685_SetServoAngle(1, 100);
+	  PCA9685_SetServoAngle(2, 110);
+	  PCA9685_SetServoAngle(3, 45);
+	  HAL_Delay(500);
+	  PCA9685_SetServoAngle(4, 60);
+	  PCA9685_SetServoAngle(5, 40);
+	  PCA9685_SetServoAngle(6, 95);
+	  PCA9685_SetServoAngle(7, 110);
+	  PCA9685_SetServoAngle(8, 75);
+	  PCA9685_SetServoAngle(9, 100);
+	  PCA9685_SetServoAngle(10, 105);
+	  PCA9685_SetServoAngle(11, 35);
+
 }
 
 void servos()
@@ -122,6 +120,61 @@ void servos()
 //		  ActiveServo++;
 //		  if (ActiveServo >= SERVO_COUNT) ActiveServo = 0;
 	  }
+	vTaskDelete(NULL);
+}
+
+void walk(void *parameters)
+{
+	while(1)
+	{
+		PCA9685_SetServoAngle(7,110-50);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(11,45 - 45);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(7,110);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(4,60+40);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(0,110-30);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(8,75-50);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(4,60);
+
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(0,110);
+			PCA9685_SetServoAngle(11,35);
+			PCA9685_SetServoAngle(9,100-45);
+			PCA9685_SetServoAngle(2,110-45);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(2,110);
+
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(10,105);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(6,95+40);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(10,145);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(6,95);
+
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(5,40+40);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(1,110-30);
+			PCA9685_SetServoAngle(9,100+5);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(5,40);
+			HAL_Delay(100);
+
+			PCA9685_SetServoAngle(1,110);
+			PCA9685_SetServoAngle(3,45+35);
+			PCA9685_SetServoAngle(10,125+10);
+			PCA9685_SetServoAngle(8,75);
+			HAL_Delay(100);
+			PCA9685_SetServoAngle(3,45);
+			HAL_Delay(100);
+	}
 	vTaskDelete(NULL);
 }
 
@@ -229,12 +282,14 @@ int main(void)
 
 
 
-  //
+  start_normal();
   xTaskHandle HT1,HT2;
   xTaskCreate(faces, "face_show", configMINIMAL_STACK_SIZE, 0, tskIDLE_PRIORITY, &HT1);
-  xTaskCreate(start_normal, "debugging", configMINIMAL_STACK_SIZE, 0, tskIDLE_PRIORITY, &HT2);
-//  servos();
+  xTaskCreate(walk, "debugging", configMINIMAL_STACK_SIZE, 0, tskIDLE_PRIORITY, &HT2);
+
    vTaskStartScheduler();
+
+
   /* USER CODE END 2 */
 
   /* Init scheduler */
@@ -274,6 +329,7 @@ int main(void)
   /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
   while (1)
   {
     /* USER CODE END WHILE */
